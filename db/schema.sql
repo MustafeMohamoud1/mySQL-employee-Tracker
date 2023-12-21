c4237910801b907cf5b0f45dffb7871ad96c8685
mustafemohamoud <mustyymo@outlook.com>
2023-12-21 18:24:17 +0000

    tables created in database

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE departments{
+    id int auto_increment primary key,
+    name varchar(30)
+};
+
+CREATE TABLE roles{
+    id int auto_increment primary key,
+    title varchar(30),
+    salary decimal,
+    department_id int
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+};
+
+CREATE TABLE employees{
+    id int auto_increment primary key,
+    firstname varchar(30),
+    lastname varchar(30),
+    role_id int
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    manager_id int
+};
